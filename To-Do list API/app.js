c03d774663f55c3588cab0ae4e66bc0aec60f08f
Irteza Ratul <ratul.xc23@gmail.com>
2025-07-08 22:38:23 +0700

    connected to mongoose

--- To-Do list API/app.js
+++ To-Do list API/app.js
@@ -5,3 +5,10 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+mongoose.connect("mongodb+srv://ratul07:Manutd_20@ratul.cskiwek.mongodb.net/ratulApp?retryWrites=true&w=majority&appName=Ratul", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Atlas connected"))
+.catch(err => console.error("MongoDB connection error:", err));
