48ce98ba370851809c82587f9dbab530a23f0924
Irteza Ratul <ratul.xc23@gmail.com>
2025-07-24 22:55:38 +0700

    not finished

---
+++ contact-manager-api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
