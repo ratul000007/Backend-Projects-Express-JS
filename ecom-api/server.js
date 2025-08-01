4888c04df0f842a29af2213d682a05d204e59b79
Irteza Ratul <ratul.xc23@gmail.com>
2025-08-01 22:57:15 +0700

    bug fixed

--- ecom-api/server.js
+++ ecom-api/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB Connected"))
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
-app.use("./api/orders", require("./routes/orderRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
