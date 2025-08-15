ebc580e21cbf0806240838b33be6745cb00aa7bf
Irteza Ratul <ratul.xc23@gmail.com>
2025-08-15 22:46:50 +0700

    userchema

--- chat-app/models/User.js
+++ chat-app/models/User.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    
-})
+    email: { type: String, required: true, sparse: true },
+    password: { type: String, required: true },
+    avatarUrl: String,
+    lastSeen: Date,
+    isOnline: { type: Boolean, default: false },
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
